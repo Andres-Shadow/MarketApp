aedfb7762f26bcc3e1e0632aececee1748f4af77
Andres-Shadow <andresm.dussanb@uqvirtual.edu.co>
2023-09-06 14:34:03 -0500

    full services implementation

--- Business/src/main/java/co/uniquindio/sinfoci/ServiceImpl/ClientServiceImpl.java
+++ Business/src/main/java/co/uniquindio/sinfoci/ServiceImpl/ClientServiceImpl.java
@@ -1,33 +1,59 @@
 package co.uniquindio.sinfoci.ServiceImpl;
 
 import co.uniquindio.sinfoci.Entities.Client;
+import co.uniquindio.sinfoci.Repositories.ClientRepository;
 import co.uniquindio.sinfoci.Services.ClientService;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ClientServiceImpl implements ClientService {
+
+    //DATA REPOSITORIES
+
+    private ClientRepository clientRepo;
+
+    private ClientServiceImpl(ClientRepository clientRepo){
+        this.clientRepo = clientRepo;
+    }
+
     @Override
     public Client createClient(Client newClient) {
-        return null;
+        Client savedClient = null;
+        savedClient = clientRepo.save(newClient);
+        return  savedClient;
     }
 
     @Override
     public void deleteCliente(Integer clientId) {
-
+        Optional<Client> found = clientRepo.findById(clientId);
+        if (!found.isEmpty()) {
+            clientRepo.delete(found.get());
+        }
     }
 
     @Override
     public Client updateClient(Client newClient) {
-        return null;
+        Optional<Client> found = clientRepo.findById(newClient.getID());
+        Client saved = null;
+        if(!found.isEmpty()){
+            saved = clientRepo.save(newClient);
+        }
+        return saved;
     }
 
     @Override
     public List<Client> listAllClients() {
-        return null;
+        return clientRepo.findAll();
     }
 
     @Override
     public Client getClient(Integer clientId) {
-        return null;
+        Optional<Client> found = clientRepo.findById(clientId);
+        if(!found.isEmpty()){
+            return found.get();
+        }else{
+            return null;
+        }
     }
 }
